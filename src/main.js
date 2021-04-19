5548fad4e2225e1272a5807586488bff2af64aba
Allison Pham <alapham@ucsc.edu>
2021-04-18 19:42:36 -0700

    Updated Updated Mods List

--- src/main.js
+++ src/main.js
@@ -12,9 +12,14 @@ let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyF, keyR;
 
-//Added countdown/timer display
-//Added new ship type
-//Parallax scrolling
-//Changed game's theme/asthetic (Artwork, Sound Effects, UI, Animation)
+
+// Allison Pham
+// Rocket Patrol Mod (Underwater Patrol)
+// Date: 4/14/21
+// Project Time: ~11 hours
+//Added countdown/timer display (10)
+//Added new ship type (20)
+//Parallax scrolling (10)
+//Changed game's theme/asthetic (Artwork, Sound Effects, UI, Animation) (60)
 //Art and animation art by me (Allison Pham)
 //Sound Effects Credit: https://freesound.org/
